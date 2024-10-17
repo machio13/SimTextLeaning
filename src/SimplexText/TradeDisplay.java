554870aa9cba17677c9618defdc578a2f58eff31
machio13 <o8078281124@gmail.com>
2024-10-17 15:40:43 +0900

    8回目コミット、text5

--- src/SimplexText/TradeDisplay.java
+++ src/SimplexText/TradeDisplay.java
@@ -24,7 +24,7 @@ public class TradeDisplay {
     public void showTrade(List<TradeStock> tradeStockList) {
         System.out.println("|========================================================================================================|");
         System.out.println("| Traded Datetime  | Product Name           | Side | Quantity.     | Traded Unit Price | Input Datetime  |");
-        System.out.println("|------------------+------------------------+------+----------+-------------------+-----------------|");
+        System.out.println("|------------------+------------------------+------+---------------+-------------------+-----------------|");
 
         for (TradeStock tradeStock : tradeStockList) {
             LocalDateTime tradedDatetime = tradeStock.getTDTime();
@@ -34,8 +34,8 @@ public class TradeDisplay {
             BigDecimal tradedUnitPrice = tradeStock.getTUPrice();
             LocalDateTime inputDatetime = tradeStock.getIDTime();
 
-            System.out.printf("| %-17s| %-23s| %-5s| %9s| %18s| %15s|\n", tradedDatetime, productName, side, quantity, tradedUnitPrice, inputDatetime);
+            System.out.printf("| %-17s| %-23s| %-5s| %14s| %18s| %15s|\n", tradedDatetime, productName, side, quantity, tradedUnitPrice, inputDatetime);
         }
-        System.out.println("|===================================================================================================|");
+        System.out.println("|========================================================================================================|");
     }
 }
--- src/SimplexText/TradeReader.java
+++ src/SimplexText/TradeReader.java
@@ -9,13 +9,15 @@ import java.nio.file.NoSuchFileException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class TradeReader {
 
     public List<TradeStock> readTrade(File csvFile) {
         List<TradeStock> tradeStockList = new ArrayList<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm");
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm");
 
         String lineSplit = ",";
         String line = "";
@@ -30,6 +32,7 @@ public class TradeReader {
                 LocalDateTime formatToday = LocalDateTime.parse(lineBox[5]);
 //                LocalDateTime formatToday = LocalDateTime.parse(lineBox[5], formatter);
                 tradeStockList.add(new TradeStock(formatDatetime, lineBox[1], lineBox[2], formatQuantity, bigDecimal, formatToday));
+
             }
         }catch (IOException e) {
             System.out.println("うまく読み込めませんでした");
